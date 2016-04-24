cacbd87447791e382cdb8340a42e022e3550ab30
Connor Smith <c3smith@ucsd.edu>
2016-04-24 04:41:32 -0700

    Lots of things work! It's great!

--- Assets/BunsenHeat.cs
+++ Assets/BunsenHeat.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class BunsenHeat : MonoBehaviour {
 
   public ParticleSystem bunsen;
+  public Object fireBurn;
   public float heatRate = 0.1f;
 
 	// Use this for initialization
@@ -37,5 +38,23 @@ public class BunsenHeat : MonoBehaviour {
         other.gameObject.GetComponent<SteelFire>().Explode();
       }
     }
+
+    else if (other.tag == "Paper") {
+
+      StartCoroutine(BurnPaper(other));
+
+    }
+  }
+
+  IEnumerator BurnPaper(Collider other) {
+
+    ((GameObject)(GameObject.Instantiate(fireBurn, other.transform.position, Quaternion.identity))).transform.parent = other.transform;
+
+    yield return new WaitForSeconds(3.0f);
+
+    other.GetComponent<Joint>().connectedBody.GetComponent<GrabScriptVive>().Disconnect();
+
+    GameObject.Destroy(other.gameObject);
+
   }
 }
---
+++ Assets/WaterFill.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterFill : MonoBehaviour {
+
+  public Object waterSource;
+  public ParticleSystem waterSystem;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  void OnTriggerEnter(Collider other) {
+
+    if (other.GetComponent<FluidHolderScript>()) {
+      if(waterSystem.emissionRate >= 5)
+        Debug.Log("FILLING");
+      StartCoroutine(fill());
+
+    }
+  }
+
+  void OnTriggerExit(Collider other) {
+
+    StopAllCoroutines();
+
+  }
+
+  IEnumerator fill() {
+
+    while(true) {
+
+      Debug.Log("COROUTINE");
+      GameObject.Instantiate(waterSource, this.transform.position, Quaternion.identity);
+
+      yield return new WaitForSeconds(0.5f);
+    }
+
+  }
+}
